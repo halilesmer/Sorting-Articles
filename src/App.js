1e0f5caa410943cf71b3e8ab63b67867d56b2987
halilesmer <halil@esmer.de>
2022-06-17 07:43:43 +0200

    initial

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import 'h8k-components';
 
@@ -6,17 +6,31 @@ import Articles from './components/Articles';
 
 const title = "Sorting Articles";
 
-function App({articles}) {
+function App({ articles }) {
+    const [sortedArticles, setsortedArticles] = useState(articles)
+    // const [article, setArticle] = useState(articles);
+
+
+    const onClickHandleUpvotes = () => {
+        return setsortedArticles(articles.sort((a, b) => (a.upvotes > b.upvotes) ? -1 : 1))
+    }
+    const onClickHandleMostRecent=()=>{
+        // return  setsortedArticles(articles.sort((a, b) => (a.dates > b.dates) ? -1 : 1))
+        return  setsortedArticles(articles.sort((a, b) => new Date(b.date).getTime() - new Date(a.date).getTime()));
+    }
+    console.log("upvotes: ", sortedArticles);
 
     return (
         <div className="App">
             <h8k-navbar header={title}></h8k-navbar>
             <div className="layout-row align-items-center justify-content-center my-20 navigation">
                 <label className="form-hint mb-0 text-uppercase font-weight-light">Sort By</label>
-                <button data-testid="most-upvoted-link" className="small">Most Upvoted</button>
-                <button data-testid="most-recent-link" className="small">Most Recent</button>
+                <button onClick={onClickHandleUpvotes} data-testid="most-upvoted-link" className="small">Most Upvoted</button>
+                <button onClick={onClickHandleMostRecent} data-testid="most-recent-link" className="small">Most Recent</button>
             </div>
-            <Articles articles={articles}/>
+            <Articles articles={articles}
+                sortedArticles={sortedArticles}
+            />
         </div>
     );
 
--- src/components/Articles.js
+++ src/components/Articles.js
@@ -1,23 +1,37 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-function Articles() {
+function Articles({ articles, sortedArticles }) {
+    console.log("sortedArticles: ", sortedArticles);
+    const [upvotes, setUpvotes] = useState(sortedArticles)
+    const [sortUpvotes, setSortUpvotes] = useState(true)
+
+
+  
 
     return (
         <div className="card w-50 mx-auto">
             <table>
                 <thead>
-                <tr>
-                    <th>Title</th>
-                    <th>Upvotes</th>
-                    <th>Date</th>
-                </tr>
+                    <tr>
+                        <th>Title</th>
+                        <th>Upvotes</th>
+                        <th>Date</th>
+                    </tr>
                 </thead>
                 <tbody>
-                <tr data-testid="article" key="article-index">
-                    <td data-testid="article-title">Article 1 title</td>
-                    <td data-testid="article-upvotes">Article 1 title</td>
-                    <td data-testid="article-date">Article 1 title</td>
-                </tr>
+                    {sortedArticles.map((article, index) => {
+                        return (
+                            <>
+                                <tr data-testid="article" key={index}>
+                                    <td  data-testid="article-title">{article.title}</td>
+                                    <td data-testid="article-upvotes">{article.upvotes}</td>
+                                    <td key={article.date} data-testid="article-date">{article.date}</td>
+                                </tr>
+                            </>
+                        )
+
+
+                    })}
                 </tbody>
             </table>
         </div>
